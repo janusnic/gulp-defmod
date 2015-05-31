460da33cff2242a75b1bc9fd133d0e2df1d023c4
aihui zhu <mr.zhuah@gmail.com>
2015-05-31 11:14:15 +0800

    add children object for module to avoid name conflict

--- moduler.js
+++ moduler.js
@@ -1,91 +1,97 @@
 var define, defineAlias, require;
 
 (function () {
-    'use strict';
-    var modules = {};
+    'use strict'
+    var modules = {
+        children: {}
+    }
+
     var state = {
         UNINIT: 0, // 未初始化
         WAITINIT: 1 // 正在等待其他模块初始化
-    };
+    }
 
     function defined(mod) {
-        return mod.exports || mod._alias || mod._initializer;
+        return mod.exports || mod.alias || mod.initializer
     }
 
     function module(parent, name, isDef) {
-        var names = name.split('/');
-        var curr;
+        var names = name.split('/')
+
+        var curr
         names.forEach(function (n, i) {
-            curr = parent[n];
+            curr = parent.children[n]
             if (curr) {
                 if (isDef && i === names.length - 1 && defined(curr)) {
-                    throw 'Module ' + name + ' already defined';
+                    throw new Error('Module ' + name + ' already defined')
                 }
             } else {
                 if (!isDef) {
-                    throw 'Module ' + name + ' not defined';
+                    throw new Error('Module ' + name + ' not defined')
                 }
 
-                curr = {};
-                parent[n] = curr;
+                curr = {
+                    children: {}
+                }
+                parent.children[n] = curr
             }
 
-            parent = curr;
-        });
+            parent = curr
+        })
 
-        return curr;
+        return curr
     }
 
     define = function (name, fn) {
-        var mod = module(modules, name, true);
+        var mod = module(modules, name, true)
 
         if (typeof fn !== 'function') {
-            mod.exports = fn;
+            mod.exports = fn
         } else {
-            mod._initializer = fn;
-            mod._state = state.UNINIT;
+            mod.initializer = fn
+            mod.state = state.UNINIT
         }
-
-    };
+    }
 
     defineAlias = function (name, alias) {
-        module(modules, name, true)._alias = alias;
-    };
+        module(modules, name, true).alias = alias
+    }
 
-    var requireIn = function(modules, name) {
-        var mod = module(modules, name);
+    var requireIn = function (modules, name) {
+        var mod = module(modules, name)
 
-        if (mod._alias) {
-            return requireIn(mod, mod._alias);
+        if (mod.alias) {
+            return requireIn(mod, mod.alias)
         }
 
         if (mod.exports) {
-            return mod.exports;
+            return mod.exports
         }
 
-        if (mod._state === state.WAITINIT) {
-            throw 'Circular dependencies of mod' + name;
+        if (mod.state === state.WAITINIT) {
+            throw new Error('Circular dependencies of mod' + name)
         }
 
-        // _state = UNINIT
-        mod._state = state.WAITINIT;
-        mod.exports = {};
-        mod._initializer(mod, mod.exports);
-        mod._state = undefined;
-        mod._initializer = undefined;
+        // state = UNINIT
+        mod.state = state.WAITINIT
+        mod.exports = {}
+        mod.initializer(mod, mod.exports)
+        delete(mod, 'state')
+        delete(mod, 'initializer')
 
-        return mod.exports;
+        return mod.exports
     }
 
     require = function (name) {
         return requireIn(modules, name)
-    };
-})();
+    }
+
+})()
 
 if (typeof window === 'undefined') {
     module.exports = {
         define: define,
-        defineAlias:defineAlias,
+        defineAlias: defineAlias,
         require: require
-    };
+    }
 }
